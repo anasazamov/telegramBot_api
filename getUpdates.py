64bfb5a471b0f800b46be9f09d693b539a4567ba
djumanov <diyorbekjumanov1120@gmail.com>
2023-03-07 19:15:25 +0500

    get updates

--- getUpdates.py
+++ getUpdates.py
@@ -1,10 +1,14 @@
 import requests
 
-TOKEN = ''
+TOKEN = '6097521187:AAHbPqQPSlFP54uT-Sj6MdnJpeBB_5Idsmg'
 BASE_URL = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
 
 
 response = requests.get(url=BASE_URL)
 updates=response.json()['result']
 
-# print each update
+last_update = updates[-1]
+last_msg = last_update['message']
+text = last_msg['text']
+first_name = last_msg['from']['first_name']
+print(text, first_name)

5664a3d8a818f9f3e87123a9f141b54ccb9d8909
anasazamov <anasazamov55@gmail.com>
2023-03-09 16:48:50 +0500

    Not stop send image

--- test.py
+++ test.py
@@ -3,11 +3,12 @@ TOKEN = '6058623152:AAGROkpnAa7tK6gXhGiE_F-DpMqqFuxK8ns'
 get_update= f'https://api.telegram.org/bot{TOKEN}/getUpdates'
 get_send= f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 
-l=0
+l=len(get(get_update).json()['result'])
 
 while True:
     l_2=len(get(get_update).json()['result'])
-    if l<l_2:
+    _key_= get(get_update).json()['result'][-1]['message'].keys()
+    if l<l_2 and  "text" in _key_:
         text= get(get_update).json()['result'][-1]["message"]['text']
         chat_id=get(get_update).json()['result'][-1]["message"]['from']["id"]
         payload = {"chat_id":chat_id,"text":text}
